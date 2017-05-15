51294cd7097d9a73532f93a83870181ad073550f
satish test repository <Satish-IOS@satishs-mac-mini.local>
2017-05-15 12:17:19 +0530

    Initial Commit

---
+++ GitPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by Satish ios on 15/05/17.
+//  Copyright © 2017 NineHertzIndia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
